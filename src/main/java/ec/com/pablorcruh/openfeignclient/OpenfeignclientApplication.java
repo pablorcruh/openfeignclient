0182100884cf2022b7fa8c37ea173b49c6c8cbf5
pablorcruh <pablorcruh@gmail.com>
2020-10-18 20:37:55 -0500

    add project setup

---
+++ src/main/java/ec/com/pablorcruh/openfeignclient/OpenfeignclientApplication.java
@@ -0,0 +1,13 @@
+package ec.com.pablorcruh.openfeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignclientApplication.class, args);
+    }
+
+}
